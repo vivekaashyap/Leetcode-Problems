7df71148dc04bbb85425bcb8eccc2938a3664047
Vivek Kashyap <vivekkashyap8299@gmail.com>
2026-05-18 07:51:42 +0530

    Time: 1 ms (95.60%) | Memory: 47 MB (7.33%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,3 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
@@ -5,32 +20,16 @@ class Solution {
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
         while(!q.isEmpty()){
+            List<Integer> lvl = new ArrayList<>();
             int size = q.size();
-            List<Integer> level = new ArrayList<>();
             for(int i = 0; i<size; i++){
                 TreeNode curr = q.poll();
-                level.add(curr.val);
-                if(curr.left!=null) q.offer(curr.left);
-                if(curr.right!=null) q.offer(curr.right);
-            }   
-            res.add(level);
+                lvl.add(curr.val);
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+            res.add(lvl);
         }
         return res;
     }
 }
-        // List<List<Integer>> res = new ArrayList<>();
-        // if(root == null) return res;
-        // Queue<TreeNode> q = new LinkedList<>();
-        // q.offer(root);
-        // while(!q.isEmpty()){
-        //     int size = q.size();
-        //     List<Integer> level = new ArrayList<>();
-        //     for(int i = 0; i<size; i++){
-        //         TreeNode curr = q.poll();
-        //         level.add(curr.val);
-        //         if(curr.left != null) q.offer(curr.left);
-        //         if(curr.right != null) q.offer(curr.right);
-        //     }
-        //     res.add(level);
-        // }
-        // return res;
